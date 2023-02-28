0c44ff5a811a10f40132a87dce99d0e98582e986
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-02-28 17:22:41 +0800

    Update lists.jsx

--- lists.jsx
+++ lists.jsx
@@ -1,7 +1,7 @@
 const numbers = [1, 2, 3, 4, 5];
 
 const updatedNums = numbers.map((number, index) => {
-    return <li key={index.toString}>{number}</li>;
+    return <li key={index.toString()}>{number}</li>;
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
