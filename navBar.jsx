df0c6823a711adbe3a0ced5684153ac629533375
Dan Liljestig <39473290+liljestig@users.noreply.github.com>
2023-02-28 18:37:42 +0800

    Update navBar.jsx

--- navBar.jsx
+++ navBar.jsx
@@ -1,9 +1,11 @@
 // use of "props" to set data
 function NavBar(props) {
     const list = props.menuitems;
+    const {Button} = ReactBootstrap;
     // each item should have a unique key
     const updatedList = list.map((listItems, index) => {
-        return <li key={index}>{listItems}</li>;
+        //return <li key={index}>{listItems}</li>;
+        return <Button key={index}>{listItems}</Button>;
     });
     // note that React needs to have a single Parent
     return <ul>{updatedList}</ul>;
